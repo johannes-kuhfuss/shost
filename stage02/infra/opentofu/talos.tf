c81c2a7aa8e85e23fc8c6dbb6b881655873e00e1
johannes-kuhfuss <johannes.kuhfuss@web.de>
2026-08-28 15:02:52 +0200

    enable ssh

--- stage02/infra/opentofu/talos.tf
+++ stage02/infra/opentofu/talos.tf
@@ -15,6 +15,11 @@ provider "proxmox" {
   endpoint  = var.proxmox_endpoint
   api_token = var.proxmox_api_token
   insecure  = true
+
+  ssh {
+    agent = true
+    usernmame = "opentofu"
+  }
 }
 
 resource "proxmox_download_file" "talos_image" {
